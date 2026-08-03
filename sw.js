31888284eda36101016d94914b2abec492426d6c
mrzunairkhalid-jpg <mrzunairkhalid@gmail.com>
2026-08-03 21:22:14 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,121 @@
+const CACHE_NAME = "money-manager-v1";
 
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./storage.js",
+  "./ui.js",
+  "./script.js",
+  "./manifest.json"
+];
+
+
+// Install
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+
+        return cache.addAll(APP_FILES);
+
+      })
+
+  );
+
+  self.skipWaiting();
+
+});
+
+
+// Activate
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+// Fetch
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+
+          return cachedResponse;
+
+        }
+
+        return fetch(event.request)
+          .then(networkResponse => {
+
+            if (
+              !networkResponse ||
+              networkResponse.status !== 200 ||
+              networkResponse.type !== "basic"
+            ) {
+
+              return networkResponse;
+
+            }
+
+            const responseClone =
+              networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+
+                cache.put(
+                  event.request,
+                  responseClone
+                );
+
+              });
+
+            return networkResponse;
+
+          })
+          .catch(() => {
+
+            return caches.match("./index.html");
+
+          });
+
+      })
+
+  );
+
+});
